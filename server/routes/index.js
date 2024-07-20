3a4625e6a16c69bbe36b600177c9998f9d1e0dbe
Seetharam <ramchowdhary73@gmail.com>
2024-07-20 22:36:21 +0530

    initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import passport from "passport";
+import AuthApi from "./AuthApi.js";
+import TransactionsApi from "./TransactionsApi.js";
+import UserApi from "./UserApi.js";
+const router = Router();
+
+const auth = passport.authenticate("jwt", { session: false });
+
+router.use("/transaction", auth, TransactionsApi);
+router.use("/auth", AuthApi);
+router.use("/user", UserApi);
+
+export default router;
